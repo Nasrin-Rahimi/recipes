ab483a72349782c4cd878b640a9cc3d699202b70
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-10-15 07:51:33 -0400

    enable sesions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #turn sessions on
+    set :session_secret, "my_best_recipes_secret"
   end
 
   get "/" do
