c120e1218d9df5d72d35e6ff0689d669b9f22bd4
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-10-15 16:50:14 -0400

    add post route to save session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,10 @@ class SessionsController < ApplicationController
     get '/login' do
         erb :"sessions/login.html"
     end
-    
+
+    post '/sessions' do 
+       session[:email] = params[:email]
+       redirect '/recipes'
+    end
+
 end
