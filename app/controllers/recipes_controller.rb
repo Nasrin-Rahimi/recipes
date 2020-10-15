a3d68044e37c380fb25608dc4a06a03379ed08cb
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-10-15 16:59:12 -0400

    create a route for new recipes and check if user login

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,19 @@
 class RecipesController < ApplicationController
 
     get '/recipes' do
-        "You are logged in as #{session[:email]}"
+        # "You are logged in as #{session[:email]}"
+        "A list of publically available Recipes."
+    end
+
+    get '/recipes/new' do
+        #checking if thay are logged in
+        if !session[:email]
+            redirect "/login"
+        else
+            "A new recipe form"
+            # erb :"recipes/new"
+        end
+
     end
 
 end
