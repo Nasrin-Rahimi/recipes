aa9009c01eb0df336292ad298930eb7433e7d620
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-10-16 06:43:38 -0400

    signup users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :"/users/new"
+    end
+
+    post '/users' do
+        #check user fields validation, if fields are not valid, stay in signup page!
+        user = User.create(:email => params[:email], :password => params[:password])
+        redirect '/login'
+    end
+
 end
