abcc742735a08b36caf4ea633f18bec456c021be
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-10-19 09:03:18 -0400

    change logout redirecting page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     get '/logout' do
         logout!
         # redirect "/login"
-        redirect "/recipes"
+        redirect "/"
     end
 
 end
