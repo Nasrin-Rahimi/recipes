21df6be99e77560eac3330b1e84e40df1ca3cf05
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-10-20 07:50:15 -0400

    add slug to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,13 @@
 class Category < ActiveRecord::Base
     has_many :recipes
+
+    def slug
+        self.name.strip.gsub(' ', '-')
+      end
+    
+      def self.find_by_slug(slug)
+        name = slug.split('-').join(' ')
+        Category.find_by(:name => name)
+      end
+
 end
